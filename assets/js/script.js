c2ebacac270fafaf345c8dfb34ee3adb62ef6d3e
Gerry M <waltermccone88@gmail.com>
2021-02-01 20:06:33 -0600

    updated HTML and added PsuedoCode to the JS file for the groups reference.

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+//Weather APP Code
+
+// Display current city in header of the Weather Element
+// Display current weather condition in the text of the Weather Element
+
+
+
+
+
+
+//Music APP Code
+//Receive the weather condition into the Playlist function
+
+
+
+
+//Background APP code
+//Create a function that receives weather condition and adds a new background based off of that condition
+
+
+
+
+
+
+
+//Form Code
+
+//Submit listener
+
+//input value is assigned a variable
+
+//Add variable to Local Storage
+
+// Display local storage to list below form
