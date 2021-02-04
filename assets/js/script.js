1d17d5d9e7231859cf40dfa8a473095a6a30bff3
Gerry M <waltermccone88@gmail.com>
2021-02-03 19:21:26 -0600

    Added an Iframe and changed the JS with soundcloud API variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,6 +59,11 @@ function searchWeatherApi(query) {
 //Music APP Code
 //Receive the weather condition into the Playlist function
 
+var iframeElement   = document.querySelector('iframe');
+var iframeElementID = iframeElement.id;
+var widget1         = SC.Widget(iframeElement);
+var widget2         = SC.Widget(iframeElementID);
+// widget1 === widget2
 
 
 
